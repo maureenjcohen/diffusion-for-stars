4bb85b92fd72e0df31a359d354643c701e4dcdff
Maureen Cohen <s1144983@sms.ed.ac.uk>
2021-10-07 10:03:49 +0100

    Added requirements.txt, formatted code to PEP8 standard, fixed potential bug if image is rectangular, removed dependency on OpenCV

--- src/diffusion.py
+++ src/diffusion.py
@@ -1,17 +1,24 @@
 # -*- coding: utf-8 -*-
 """
 @author: Maureen Cohen, University of Edinburgh
-@updated: 18 October 2020
+@updated: 7 October 2021
 
-Removes background stars from astronomical images using a diffusion-based method.
+Removes background stars from astronomical images using a diffusion-based
+method.
 
-This program was written in the course of a Master of Physics research project at
-Heriot-Watt University, Edinburgh, under the supervision of Dr Weiping Lu.
+This program was written in the course of a Master of Physics research project
+at Heriot-Watt University, Edinburgh, under the supervision of Dr Weiping Lu.
 
-References: 
-Perona, P. & Malik, J. Scale-Space and Edge Detection Using Anisotropic Diffusion. IEEE Transactions on Pattern Analysis and Machine Intelligence 12 (7), 629-639 (1990)
-Chao, S. & Tsai, D. Astronomical Image Restoration Using an Improved Anisotropic Diffusion. Pattern Recognition Letters 27, 335-344 (2006)
-Muldal, A. Implementation of Perona-Malik diffusion in Python. https://pastebin.com/sBsPX4Y7 (2012)
+References:
+Perona, P. & Malik, J. Scale-Space and Edge Detection Using Anisotropic
+Diffusion. IEEE Transactions on Pattern Analysis and Machine Intelligence 12
+(7), 629-639 (1990)
+
+Chao, S. & Tsai, D. Astronomical Image Restoration Using an Improved
+Anisotropic Diffusion. Pattern Recognition Letters 27, 335-344 (2006)
+
+Muldal, A. Implementation of Perona-Malik diffusion in Python.
+https://pastebin.com/sBsPX4Y7 (2012)
 
 """
 
@@ -21,169 +28,204 @@ import scipy.ndimage as sc
 from skimage.measure import label
 
 """ Initialise parameters """
-avg_background_intensity = 46 
+avg_background_intensity = 46
 # Average background intensity of image to be processed, to be empirically
 # determined before processing. 46 is the average background intensity of the
 # synthetic image created to test the algorithm
-neighbourhood_size = 24 
-# Size of region in pixels to be sampled to determine whether a pixel is in a signal
-# region or background region. This value can be changed without significant
-# impact on the processed image. 
+neighbourhood_size = 24
+# Size of region in pixels to be sampled to determine whether a pixel is in a
+# signal region or background region. This value can be changed without
+# significant impact on the processed image.
 signal_intensity_threshold = 2.5
-# Regions of the image 2.5 times brighter than the background are defined as part
-# of the signal - a nebula, streak, etc.
+# Regions of the image 2.5 times brighter than the background are defined as
+# part of the signal - a nebula, streak, etc.
 signal_size_threshold = 50
-# Size threshold in pixels for considering a connected region in the relative residue
-# to be signal rather than star/noise. Should be chosen to reflect the typical
-# scale of signal features in the image.
+# Size threshold in pixels for considering a connected region in the relative
+# residue to be signal rather than star/noise. Should be chosen to reflect the
+# typical scale of signal features in the image.
 
 
 def make_binary(original_img, relative_residue, iteration):
-    """ This function creates the 'local stop' by transforming the relative 
-    residue into a binary image. A 1 in the local stop means that the corresponding
-    pixel of the image being processed will be modified at the next time step,
-    while a 0 indicates that the pixel intensity value will not change. 
-    The local stop is updated at each iteration.
-    
-    Input: Original image to be processed, relative residue, iteration of algorithm
-    Output: A binary image called the local stop which controls the next iteration
+    """ This function creates the 'local stop' by transforming the relative
+    residue into a binary image. A 1 in the local stop means that the
+    corresponding pixel of the image being processed will be modified at the
+    next time step while a 0 indicates that the pixel intensity value will not
+    change. The local stop is updated at each iteration.
+
+    Input: Original image to be processed, relative residue, iteration of
+    algorithm
+    Output: A binary image called the local stop which controls the next
+    iteration
     of the algorithm """
-    
-    img = np.float64(relative_residue.copy()) # Copies relative residue array for processing
-    img[img < 0 ] = 0 # Sets negative values to 0
-    
+
+    # Copies relative residue array for processing
+    img = np.float64(relative_residue.copy())
+    img[img < 0] = 0  # Sets negative values to 0
+
     # The section of code below pinpoints pixels in the signal region of the
     # original image where large changes are being made at the specified
     # iteration.
 
-    signal = [] # Initialises list of pixels judged by the algorithm to be signal
-    neighbourhood = sc.uniform_filter(original_img, neighbourhood_size, mode='reflect')/avg_background_intensity
-    # Calculates average value in a neighbourhood around each pixel in the original
-    # image and divides it by the average background value to determine whether
-    # the pixel is in a signal region or background region. A high ratio means
-    # we are in a signal region; a ratio of 1 means we are in a background region.
-    
-    for i in range(0, len(img[0])):
-        for j in range(0, len(img[1])):
-            if neighbourhood[i,j] >= signal_intensity_threshold: 
-                signal.append(img[i,j])
+    signal = []  # Initialises list of pixels judged by the algorithm to be
+    # signal
+    neighbourhood = sc.uniform_filter(
+        original_img, neighbourhood_size, mode='reflect')/avg_background_intensity
+    # Calculates average value in a neighbourhood around each pixel in the
+    # original image and divides it by the average background value to
+    # determine whether the pixel is in a signal region or background region.
+    # A high ratio means we are in a signal region; a ratio of 1 means we are
+    # in a background region.
+
+    xdim, ydim = img.shape
+    for i in range(0, xdim):
+        for j in range(0, ydim):
+            if neighbourhood[i, j] >= signal_intensity_threshold:
+                signal.append(img[i, j])
     # For loop checks whether each pixel in the relative residue is in a signal
-    # region or background region of the original image and assigns signal 
+    # region or background region of the original image and assigns signal
     # values to the signal list
-                
-    avg_value = sum(signal)/len(signal) # Calculates average intensity of the signal appearing in the relative residue
-    img[img > avg_value] = 1 # Sets relative residue to 1 if value is greater than average
-    img[img <= avg_value] = 0 
+
+    # Calculates average intensity of the signal appearing in the relative
+    # residue
+    avg_value = sum(signal)/len(signal)
+    # Sets relative residue to 1 if value is greater than average
+    img[img > avg_value] = 1
+    img[img <= avg_value] = 0
     # Sets relative residue to 0 if value is less than average. This background
     # is ignored when labelling connected regions.
-    
-    # The section of code below checks connected regions where large changes are
-    # being made to the original image at the specified iteration and decides 
-    # whether these regions are signal or a star based on a size threshold. 
+
+    # The section of code below checks connected regions where large changes
+    # are being made to the original image at the specified iteration and
+    # decides whether these regions are signal or a star based on a size
+    # threshold.
     # Signal regions are then protected while stars may continue to be smoothed
     # away at the next iteration.
-    
+
     stop = np.ones_like(img)
-        
-    labelled = label(img, connectivity=2) # Labels connected pixels in the relative residue by an integer starting from 1, 2, 3...
-    regions = np.amax(labelled) # Number of connected regions
-        
-    count_list = [] # Initialises list to count how many pixels are in each labelled region
-    for i in range(1,(regions+1)):
-        count = np.count_nonzero(labelled==i) # Counts number of pixels in a connected region i
-        count_list.append(count) 
-        
+
+    # Labels connected pixels in the relative residue by an integer starting
+    # from 1, 2, 3...
+    labelled = label(img, connectivity=2)
+    regions = np.amax(labelled)  # Number of connected regions
+
+    count_list = []  # Initialises list to count how many pixels are in each
+    # labelled region
+    for i in range(1, (regions+1)):
+        # Counts number of pixels in a connected region i
+        count = np.count_nonzero(labelled == i)
+        count_list.append(count)
+
     local_stop = stop + labelled
-    for j in range(0,len(count_list)):
+    for j in range(0, len(count_list)):
         if count_list[j] >= signal_size_threshold:
-            local_stop[local_stop==(j+2)] = 0
-            # If a connected region contains enough pixels that it must be signal,
-            # the local stop is set to 0, stopping diffusion in this region.
+            local_stop[local_stop == (j+2)] = 0
+            # If a connected region contains enough pixels that it must be
+            # signal, the local stop is set to 0, stopping diffusion in this
+            # region.
         else:
-            local_stop[local_stop==(j+2)] = 1
+            local_stop[local_stop == (j+2)] = 1
             # Otherwise, the local stop is set to 1 and diffusion can proceed
-    for i in range(0,len(img[0])):
-        for j in range(0,len(img[1])):
-            if neighbourhood[i,j] < 1.5:
-                local_stop[i,j] = 1
-                # The local stop is set to 1 in regions considered background, as
-                # these can be diffused indefinitely without compromising the signal
-    
+
+    for i in range(0, xdim):
+        for j in range(0, ydim):
+            if neighbourhood[i, j] < 1.5:
+                local_stop[i, j] = 1
+                # The local stop is set to 1 in regions considered background,
+                # as these can be diffused indefinitely without compromising
+                # the signal
+
     return local_stop
 
 
-
-def remove_stars(image_in, nt, gamma=0.25, step=(1.,1.)):
+def remove_stars(image_in, nt, gamma=0.25, step=(1., 1.)):
     """ This function removes background stars from the input image using
     a diffusion-based method with a novel diffusion coefficient. """
-        
-    img = np.float64(np.copy(image_in)) # Copies image for processing
-    delta_x, delta_y = np.zeros_like(img), np.zeros_like(img) # Initialises matrices for x- and y-gradients
-    S, E, NS, EW = np.zeros_like(img), np.zeros_like(img), np.zeros_like(img), np.zeros_like(img)
-    diffusion_coefficient = np.ones_like(img) # Initialises matrix for diffusion coefficient
-     
-    asym, asym0, asym1, asym2, asym3, asym4, asym5 = np.ones_like(img), np.ones_like(img), np.ones_like(img), np.ones_like(img), np.ones_like(img), np.ones_like(img), np.ones_like(img)
+
+    img = np.float64(np.copy(image_in))  # Copies image for processing
+    delta_x, delta_y = np.zeros_like(img), np.zeros_like(
+        img)  # Initialises matrices for x- and y-gradients
+    S, E, NS, EW = np.zeros_like(img), np.zeros_like(
+        img), np.zeros_like(img), np.zeros_like(img)
+    # Initialises matrix for diffusion coefficient
+    diffusion_coefficient = np.ones_like(img)
+
+    asym, asym0, asym1, asym2, asym3, asym4, asym5 = np.ones_like(img), np.ones_like(img), np.ones_like(
+        img), np.ones_like(img), np.ones_like(img), np.ones_like(img), np.ones_like(img)
     # Initialises matrices for asymmetry parameter at different window sizes
-    asym[1:-1,1:-1] = np.abs(img[2:,1:-1]-img[:-2,1:-1]) + np.abs(img[1:-1,2:]-img[1:-1,:-2]) + np.abs(img[2:,:-2]-img[:-2,2:]) + np.abs(img[2:,2:]-img[:-2,:-2])
+    asym[1:-1, 1:-1] = np.abs(img[2:, 1:-1]-img[:-2, 1:-1]) + np.abs(img[1:-1, 2:]-img[1:-1, :-2]) + \
+        np.abs(img[2:, :-2]-img[:-2, 2:]) + np.abs(img[2:, 2:]-img[:-2, :-2])
     # Calculates local asymmetry of original, unprocessed image
-    avg_asym = np.mean(asym) # Calculates average local asymmetry of original image
-    kf = 2*avg_asym # Fixes normalisation constant using initial average local asymmetry
-    
+    # Calculates average local asymmetry of original image
+    avg_asym = np.mean(asym)
+    kf = 2*avg_asym  # Fixes normalisation constant using initial average local
+    # asymmetry
+
     orig_img = img.copy()
     avg_orig = sc.uniform_filter(orig_img, 3, mode='reflect')
-    binary_stop = np.ones_like(img) 
+    binary_stop = np.ones_like(img)
     iteration = 0
     res_list = []
     second_res_list = []
     # For loop initialisations
-    
+
     for timestep in range(nt):
 
-        iteration += 1 # Updates iteration count
-        delta_y[:-1,:] = np.diff(img,axis=0) # N-th discrete difference along x-axis
-        delta_x[:,:-1] = np.diff(img,axis=1) # N-th discrete difference along y-axis
+        iteration += 1  # Updates iteration count
+        # N-th discrete difference along x-axis
+        delta_y[:-1, :] = np.diff(img, axis=0)
+        # N-th discrete difference along y-axis
+        delta_x[:, :-1] = np.diff(img, axis=1)
 
         avg_img = sc.uniform_filter(img, 3, mode='reflect')
-                
-        asym1[1:-1,1:-1] = np.abs(img[2:,1:-1]-img[:-2,1:-1]) + np.abs(img[1:-1,2:]-img[1:-1,:-2]) + np.abs(img[2:,:-2]-img[:-2,2:]) + np.abs(img[2:,2:]-img[:-2,:-2])
-        asym2[2:-2,2:-2] = np.abs(img[4:,2:-2]-img[:-4,2:-2]) + np.abs(img[2:-2,:-4]-img[2:-2,4:]) + np.abs(img[4:,:-4]-img[:-4,4:]) + np.abs(img[4:,4:]-img[:-4,:-4])
-        asym3[3:-3,3:-3] = np.abs(img[6:,3:-3]-img[:-6,3:-3]) + np.abs(img[3:-3,:-6]-img[3:-3,6:]) + np.abs(img[6:,:-6]-img[:-6,6:]) + np.abs(img[6:,6:]-img[:-6,:-6])
-        asym4[4:-4,4:-4] = np.abs(img[8:,4:-4]-img[:-8,4:-4]) + np.abs(img[4:-4,:-8]-img[4:-4,8:]) + np.abs(img[8:,:-8]-img[:-8,8:]) + np.abs(img[8:,8:]-img[:-8,:-8])
-        asym5[5:-5,5:-5] = np.abs(img[10:,5:-5]-img[:-10,5:-5]) + np.abs(img[5:-5,:-10]-img[5:-5,10:]) + np.abs(img[10:,:-10]-img[:-10,10:]) + np.abs(img[10:,10:]-img[:-10,:-10])
+
+        asym1[1:-1, 1:-1] = np.abs(img[2:, 1:-1]-img[:-2, 1:-1]) + np.abs(img[1:-1, 2:]-img[1:-1, :-2]) + \
+            np.abs(img[2:, :-2]-img[:-2, 2:]) + \
+            np.abs(img[2:, 2:]-img[:-2, :-2])
+        asym2[2:-2, 2:-2] = np.abs(img[4:, 2:-2]-img[:-4, 2:-2]) + np.abs(img[2:-2, :-4] -
+                                                                          img[2:-2, 4:]) + np.abs(img[4:, :-4]-img[:-4, 4:]) + np.abs(img[4:, 4:]-img[:-4, :-4])
+        asym3[3:-3, 3:-3] = np.abs(img[6:, 3:-3]-img[:-6, 3:-3]) + np.abs(img[3:-3, :-6] -
+                                                                          img[3:-3, 6:]) + np.abs(img[6:, :-6]-img[:-6, 6:]) + np.abs(img[6:, 6:]-img[:-6, :-6])
+        asym4[4:-4, 4:-4] = np.abs(img[8:, 4:-4]-img[:-8, 4:-4]) + np.abs(img[4:-4, :-8] -
+                                                                          img[4:-4, 8:]) + np.abs(img[8:, :-8]-img[:-8, 8:]) + np.abs(img[8:, 8:]-img[:-8, :-8])
+        asym5[5:-5, 5:-5] = np.abs(img[10:, 5:-5]-img[:-10, 5:-5]) + np.abs(img[5:-5, :-10]-img[5:-5, 10:]) + \
+            np.abs(img[10:, :-10]-img[:-10, 10:]) + \
+            np.abs(img[10:, 10:]-img[:-10, :-10])
         # Calculates local asymmetry in 5 window sizes
-        
+
         asymavg = ((asym1+asym2+asym3+asym4+asym5)/5)
         # Averages local asymmetry over 5 window sizes
-        
+
         if iteration <= 2:
             asym0 = asymavg*np.exp(((avg_img/avg_background_intensity)-1))
             # Weights average local asymmetry by enhancement factor
         else:
-            asym0 = asymavg*np.exp(((avg_img/avg_background_intensity)/(binary_stop)-1))
-            # From iteration 3, weights average local asymmetry by enhancement factor
-            # and applies local stop constructed using the make_binary function above
-                
-        diffusion_coefficient = (1./(1. +(asym0/kf))/step[0]) 
+            asym0 = asymavg * \
+                np.exp(((avg_img/avg_background_intensity)/(binary_stop)-1))
+            # From iteration 3, weights average local asymmetry by enhancement
+            # factor and applies local stop constructed using the make_binary
+            # function above
+
+        diffusion_coefficient = (1./(1. + (asym0/kf))/step[0])
         # Calculates diffusion coefficient
- 
+
         S[:] = diffusion_coefficient*delta_y
         E[:] = diffusion_coefficient*delta_x
         # Multiplies image gradient by new diffusion coefficient
-            
+
         NS[:] = S
         EW[:] = E
-        NS[1:,:] -= S[:-1,:]
-        EW[:,1:] -= E[:,:-1]  
+        NS[1:, :] -= S[:-1, :]
+        EW[:, 1:] -= E[:, :-1]
         # Subtracts a copy that has been shifted 'North/West' by one
-        # pixel. Don't ask questions. Just do it. Trust me. 
-        # (comment courtesy of Muldal 2012)                  
-        
-        img += gamma*(NS+EW) # Updates image
-        
+        # pixel. Don't ask questions. Just do it. Trust me.
+        # (comment courtesy of Muldal 2012)
+
+        img += gamma*(NS+EW)  # Updates image
+
         avg_imgnew = sc.uniform_filter(img, 3, mode='reflect')
         res = np.abs(avg_orig - avg_imgnew)
         res_list.append(res)
-        # Calculates residue by differencing original image and updated image 
+        # Calculates residue by differencing original image and updated image
         # Stores residue in a list
 
         if iteration > 1:
@@ -192,10 +234,11 @@ def remove_stars(image_in, nt, gamma=0.25, step=(1.,1.)):
             # Calculates relative residue by differencing two absolute residues
             # Stores relative residue in a list
         if iteration > 4:
-            avg_second_res = (second_res_list[iteration-2] + second_res_list[iteration-3] + second_res_list[iteration-4] + second_res_list[iteration-5])/4
-            binary_stop = make_binary(orig_img, avg_second_res, iteration)  
+            avg_second_res = (second_res_list[iteration-2] + second_res_list[iteration-3] +
+                              second_res_list[iteration-4] + second_res_list[iteration-5])/4
+            binary_stop = make_binary(orig_img, avg_second_res, iteration)
             # Average relative residues of preceding 4 iterations
-            # Inputs averaged relative residue to make_binary function to construct
-            # the local stop                           
+            # Inputs averaged relative residue to make_binary function to
+            # construct the local stop
 
     return img
--- src/synthetic_data_final.py
+++ src/synthetic_data_final.py
@@ -4,69 +4,85 @@
 Created on Sun Mar 10 22:16:37 2019
 
 @author: Maureen Cohen, University of Edinburgh
-@updated: 23 November 2020
+@updated: 7 October 2021
+
+This script creates the 200x200 synthetic image used as a test data set for
+the algorithm. The input images and the output synthetic image are found in
+the /img directory.
 
 """
 from __future__ import division
+from skimage import io
 import numpy as np
-import cv2
+
 
 def add_gaussian_noise(image_in, noise_sigma):
-    
     """This function adds Gaussian noise to a grayscale or colour image.
-    
+
     Inputs: Clean image, Gaussian noise sigma
     Output: Noisy image                                             """
-    
-    temp_image = np.float64(np.copy(image_in)) # Copy image, convert to float64 data type, assign to new variable
 
-    h = temp_image.shape[0] # Number of rows of temp_image is the height
-    w = temp_image.shape[1] # Number of columns of temp_image is the width
-    noise = np.random.randn(h, w) * noise_sigma # Array of random numbers with same dimensions as temp_image, multiplied by sigma. St dev is 1, mean is 0
+    # Copy image, convert to float64 data type, assign to new variable
+    temp_image = np.float64(np.copy(image_in))
 
-    noisy_image = np.zeros(temp_image.shape, np.float64) # Array of zeros with same dimensions as temp_image and data type float64
-    if len(temp_image.shape) == 2: # If image is grayscale (only first 2 arguments of .shape attribute are defined)  
-        noisy_image = temp_image + noise # Add noise to image copy
-    else: # If image has more arguments
-        noisy_image[:,:,0] = temp_image[:,:,0] + noise # Add noise to all elements in blue channel
-        noisy_image[:,:,1] = temp_image[:,:,1] + noise # Add noise to all elements in green channel
-        noisy_image[:,:,2] = temp_image[:,:,2] + noise # Add noise to all elements in red channel
+    h = temp_image.shape[0]  # Number of rows of temp_image is the height
+    w = temp_image.shape[1]  # Number of columns of temp_image is the width
+    # Array of random numbers with same dimensions as temp_image, multiplied by
+    # sigma. St dev is 1, mean is 0
+    noise = np.random.randn(h, w) * noise_sigma
 
-    return noisy_image # Output copy of image with Gaussian noise added
+    # Array of zeros with same dimensions as temp_image and data type float64
+    noisy_image = np.zeros(temp_image.shape, np.float64)
+    # If image is grayscale (only first 2 arguments of .shape attribute are
+    # defined)
+    if len(temp_image.shape) == 2:
+        noisy_image = temp_image + noise  # Add noise to image copy
+    else:  # If image has more arguments
+        # Add noise to all elements in blue channel
+        noisy_image[:, :, 0] = temp_image[:, :, 0] + noise
+        # Add noise to all elements in green channel
+        noisy_image[:, :, 1] = temp_image[:, :, 1] + noise
+        # Add noise to all elements in red channel
+        noisy_image[:, :, 2] = temp_image[:, :, 2] + noise
+
+    return noisy_image  # Output copy of image with Gaussian noise added
+
 
 """Read in a clean image, set the background value, and add noise"""
 
-nebula = cv2.imread('/path/to/nebula') # Read in clean image
-nebula = cv2.cvtColor(nebula, cv2.COLOR_BGR2GRAY) # Convert to grayscale
+nebula = io.imread('/path/to/nebula/img/figure2a.jpg', as_gray=True)
+# Read in starless nebula as grayscale image
 
-background = np.zeros([200,200], dtype=np.float64) # Empty background
-background[:] = 46 # Set a background value taken from e.g. similar natural images
-background = add_gaussian_noise(background, 17.1) # Add Gaussian noise with 
+background = np.zeros([nebula.shape[0], nebula.shape[1]], dtype=np.float64)
+# Empty background
+
+# Set a background value taken from e.g. similar natural images
+background[:] = 46
+background = add_gaussian_noise(background, 17.1)  # Add Gaussian noise with
 # sigma calculated from noise level of e.g. similar natural images
 
 """ Add background and noise to clean image"""
 
 for i in range(len(nebula[0])):
     for j in range(len(nebula[1])):
-        if nebula[i,j] < background[i,j]:
-            nebula[i,j] = background[i,j]
+        if nebula[i, j] < background[i, j]:
+            nebula[i, j] = background[i, j]
             # Add background and noise behind the image
-            
+
 """ Add stars to noisy image """
 
 starry_cloud = np.float64(nebula.copy())
-stars = cv2.imread('path/to/stars') # Read in stars
-stars = cv2.cvtColor(stars, cv2.COLOR_BGR2GRAY) # Convert to grayscale
- 
+stars = io.imread('path/to/stars/img/figure2c.jpg', as_gray=True)
+# Read in stars as grayscale image
+
 for i in range(len(starry_cloud[0])):
     for j in range(len(starry_cloud[1])):
-        if starry_cloud[i,j] + stars[i,j] > 255:
-            starry_cloud[i,j] = 255
+        if starry_cloud[i, j] + stars[i, j] > 255:
+            starry_cloud[i, j] = 255
         # If adding oversaturates image, clip at 255
         else:
-            starry_cloud[i,j] = starry_cloud[i,j] + stars[i,j]
+            starry_cloud[i, j] = starry_cloud[i, j] + stars[i, j]
         # If not, simply add the two images
 
-cv2.imwrite('/path/to/output', starry_cloud)
+io.imsave('/path/to/output', starry_cloud)
 # Save image
-
